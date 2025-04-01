e53a21f73985dac61aeeb8d8451174aea506eb38
MSib <msib.dev@mail.ru>
2025-04-01 14:08:49 +0500

    fix

---
+++ docs/assets/NotFound-CrrwBlzo.js
@@ -0,0 +1,1 @@
+import{_ as e,a as c,b as n}from"./index-C8V-Lo_1.js";const o={};function t(r,a){return n(),c("div",null,"Страница не найдена")}const _=e(o,[["render",t]]);export{_ as default};
--- docs/assets/NotFound-yrPLx6sw.js
+++
@@ -1,1 +0,0 @@
-import{_ as e,a as c,b as n}from"./index-DbRcxJmc.js";const o={};function t(r,a){return n(),c("div",null,"Страница не найдена")}const _=e(o,[["render",t]]);export{_ as default};
